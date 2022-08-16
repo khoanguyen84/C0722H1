8ddb7eae91ecb7230559b0babe5ee6d018ea2e76
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-08-16 09:59:39 +0700

    loop

---
+++ Module_1/Loop/for.js
@@ -0,0 +1,34 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+// let i = 1;
+// for(; ; ){
+//     if(i > 10){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// console.log('i outsite for', i);
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 != 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 2; i <= 10; i+=2){
+//     console.log(i);
+// }
+
+for(let i = 10; i >= 1; i--){
+    console.log(i);
+}
